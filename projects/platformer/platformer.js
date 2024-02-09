d662379ca7140e7854c4b961fd7b9582a19b2009
temple841 <157516725+temple841@users.noreply.github.com>
2024-02-09 17:08:39 +0000

    add index.html and css files

--- projects/platformer/platformer.js
+++ projects/platformer/platformer.js
@@ -52,6 +52,7 @@ $(function () {
     // You must decide on the collectable type, the x position, the y position, the gravity, and the bounce strength
     // Your collectable choices are 'database' 'diamond' 'grace' 'kennedi' 'max' and 'steve'; more can be added if you wish
     // example usage: createCollectable(type, x, y, gravity, bounce)
+    createCollectable(type)
 
 
 
